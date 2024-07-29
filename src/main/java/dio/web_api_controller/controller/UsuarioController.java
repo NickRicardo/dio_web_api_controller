13c3afe11672841292d7e76fb9c1e50c10f0276c
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-07-29 15:09:04 -0300

    First commit

---
+++ src/main/java/dio/web_api_controller/controller/UsuarioController.java
@@ -0,0 +1,38 @@
+package dio.web_api_controller.controller;
+
+import dio.web_api_controller.model.Usuario;
+import dio.web_api_controller.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @GetMapping()
+    public List<Usuario> getUser() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{username}")
+    public Usuario getOne(@PathVariable("username") String username){
+        return repository.findByUsername(username);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(Integer id){
+        repository.deleteById(id);
+    }
+
+    @PostMapping()
+    public void postUser(Usuario usuario){
+        repository.save(usuario);
+    }
+
+}
+
---
+++ src/test/java/dio/web_api_controller/WebApiControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.web_api_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
